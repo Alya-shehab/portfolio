e7bdf8e3242a53b54f558428876694da685f96f5
Alya <shehabalya@gmail.com>
2025-11-08 19:41:54 +0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+// Fade-in on scroll
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) entry.target.classList.add('visible');
+  });
+}, { threshold: 0.2 });
+sections.forEach(sec => observer.observe(sec));
